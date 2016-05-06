5bfc9b6215bdbf507549f4071de4d9ea33f974c2
Michael B <dev@mihael.me>
2016-05-06 18:17:44 +0300

    start to implement messenger

---
+++ app/src/main/java/me/mihael/messenger/Register.java
@@ -0,0 +1,22 @@
+package me.mihael.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    public void createPassword() {
+
+    }
+}
